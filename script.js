dd09ccbadcfdda2a024be001cfecf5fc1238fdb6
princid <princid85@gmail.com>
2023-08-13 18:38:44 +0530

    storing history through cookies in a function and displaying it in my history box

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 
+let equalBtn = document.getElementById("equalBtn");
 let answer = document.getElementById("answer");
+let recentHistory = document.getElementById("recentHistory");
 
 const display = (number) => {
-    answer.value += number; 
+    answer.value += number;
+
+    //Here I'm storing the input numbers inside a temp variable.
+    temp = answer.value;
+    // console.log(temp);
 }
 
+
 const calculate = () => {
-    let final_num = answer.value;
-    let final_answer = eval(final_num);
+    let final_answer = eval(answer.value);
     answer.value = final_answer;
+    // console.log(typeof(answer.value));
 }
 
 const cls = () => {
@@ -19,8 +26,17 @@ const back = () => {
     answer.value = answer.value.slice(0, -1);
 }
 
-const percent = () => {
-    let percent_value = answer.value;
-    let check_percent = percent_value.includes("%");
-    answer.value = check_percent;
+const historyFunc = () => {
+    // document.cookie = "username=Prince; expires=Tue, 15 Aug 2023 12:00:00 UTC; path=/";
+    // document.cookie = "email=Prince@gmail.com";
+    document.cookie = temp + "=" + answer.value;
+    let cookie = document.cookie;
+    recentHistory.innerText = cookie;
+
+    // For displaying the result inside an input box. But not sure, how to make editable outputs, so putting it in To-Do. 
+
+    // recentHistory.value = cookie;
+    
 }
+
+equalBtn.addEventListener('click', historyFunc);
